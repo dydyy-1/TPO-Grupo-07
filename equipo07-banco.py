f6a98bbb97538d39f49f50922f32f1aaa5af63b8
Dayana <dpenaloza@uade.edu.ar>
2025-06-05 16:04:06 -0300

    Estructura de menu finalizada.

--- equipo07-banco.py
+++ equipo07-banco.py
@@ -1,26 +1,32 @@
-usuariosID = [-1]
-contrasenas = ["admin"]
+import random
 
-def RegistrarUsuario(listaUsuarios, listaContrasenas):
+def RegistrarUsuario(listaUsuarios, listaContrasenas, listaNombres):
     cantidad = int(input("Ingrese la cantidad de usuarios para registrar: "))
     cont = 0
+    
     while cantidad <= 0:
         print("Error. La cantidad tiene que se mayor a 0.")
         cantidad = int(input("Ingrese la cantidad de usuarios para registrar: "))              
 
     while cont < cantidad:
         id = int(input("Ingrese el id del usuario a registrar (cuatro dígitos): "))
-        while id < 1000 or id > 9999:
+        while id < 1000 or id > 9999: #Validamos que sea de 4 dígitos
             id = int(input("Error. Ingrese el id del usuario a registrar (cuatro dígitos): "))
+        
         listaUsuarios.append(id)
         contrasena = str(input("Ingrese la contraseña del usuario a registrar (mínimo 5 caracteres): "))
-        while len(contrasena) < 5:
+        
+        #Validación contraseña:
+        while len(contrasena) < 5: #Validamos que tenga 5 dígitos
             contrasena = str(input("Error. Ingrese la contraseña del usuario a registrar: (mínimo 5 caracteres): "))
+        
+        nombre=input("Ingrese su nombre y apellido: ")
+        listaNombres.append(nombre)
         listaContrasenas.append(contrasena)
         cont += 1
                            
 
-def login(listaUsuarios, listaContrasenas):
+def login(listaUsuarios, listaContrasenas, listaNombres):
     usuarioExiste = 0
     claveIncorrecta = 1
 
@@ -37,51 +43,103 @@ def login(listaUsuarios, listaContrasenas):
         preguntarContrasenia = str(input("Ingrese su contraseña: "))
         if preguntarContrasenia == listaContrasenas[indexUsuario]:
             claveIncorrecta = 0
-            print("Contraseña correcta. Bienvenido/a",
-                  listaUsuarios[indexUsuario], "!")
+            print("Contraseña correcta. Bienvenido/a", listaNombres[indexUsuario], "!")
         else:
             print("Error. Contraseña incorrecta.")
 
-
-login(usuariosID, contrasenas)
-
 def menuUsuarios():
-    print("Abriendo menú de usuarios...")
-    print("\n Menu de usuarios")
-    print(" Menú de opciones: \n 0. Volver atrás. \n 1. Registrar usuario \n 2. Eliminar usuarios \n 3. Modificar datos del usuario")
-    opcionMenu = int(input("Ingrese el número correspondiente para acceder: "))  #Variable para finalizar programa o volver para atrás
+    print("\n Abriendo menú de usuarios...")
+    print("Menú de gestión de usuarios: \n 0. Volver al menú principal \n 1. Registrar usuario \n 2. Eliminar usuario \n 3. Modificar datos de un usuario \n 4. Ver todos los usuarios")
+    opcionMenu = int(input("Ingrese el número correspondiente para acceder: ")) 
     while opcionMenu != 0:
-        while opcionMenu != 0 and (opcionMenu < 1 or opcionMenu > 3): #Validación de la opción de Menú
+        while opcionMenu != 0 and (opcionMenu < 1 or opcionMenu > 4): #Validación de la opción de Menú
             if opcionMenu != 0:
                 print("\n Error. Seleccione una de las opciones siguientes.")
-                print(" Menú de opciones: \n 0. Volver atrás. \n 1. Registrar usuario \n 2. Eliminar usuarios \n 3. Modificar datos del usuario")
+                print("Menú de gestión de usuarios: \n 0. Volver al menú principal \n 1. Registrar usuario \n 2. Eliminar usuario \n 3. Modificar datos de un usuario \n 4. Ver todos los usuarios")
                 opcionMenu = int(input("Ingrese el número correspondiente para acceder: "))
         
+        #Luego de validar opciones:
         if opcionMenu == 1:
-            RegistrarUsuario(usuariosID, contrasenas)
+            RegistrarUsuario(idUsuarios, contrasenasUsuarios,nombreUsuarios)
+        elif opcionMenu == 2:
+            print("")
+        elif opcionMenu == 3:
+            print("")
+        elif opcionMenu == 4:
+            print("")
+        
+        # Cuando se terminan las otras funciones:
+        print("\n Abriendo menú de usuarios...")
+        print("Menú de gestión de usuarios: \n 0. Volver al menú principal \n 1. Registrar usuario \n 2. Eliminar usuario \n 3. Modificar datos de un usuario \n 4. Ver todos los usuarios")
+        opcionMenu = int(input("Ingrese el número correspondiente para acceder: "))
+    
+    print("Volviendo para atrás... \n")
+
+def menuTurnos():
+    print("\n Abriendo menú de turnos...")
+    print("Menú de gestión de turnos: \n 0. Volver al menú principal \n 1. Registrar nuevo turno \n 2. Finalizar turno \n 3. Ver turnos activos \n 4. Ver turnos finalizados \n 5. Ver estadísticas")
+    opcionMenu = int(input("Ingrese el número correspondiente para acceder: ")) 
+    while opcionMenu != 0:
+        while opcionMenu != 0 and (opcionMenu < 1 or opcionMenu > 5): #Validación de la opción de Menú
+            if opcionMenu != 0:
+                print("\n Error. Seleccione una de las opciones siguientes.")
+                print("Menú de gestión de turnos: \n 0. Volver al menú principal \n 1. Registrar nuevo turno \n 2. Finalizar turno \n 3. Ver turnos activos \n 4. Ver turnos finalizados \n 5. Ver estadísticas")
+                opcionMenu = int(input("Ingrese el número correspondiente para acceder: "))
+        
+        #Luego de validar opciones:
+        if opcionMenu == 1:
+            print("")
+        elif opcionMenu == 2:
+            print("")
+        elif opcionMenu == 3:
+            print("")
+        elif opcionMenu == 4:
+            print("")
+        elif opcionMenu == 5:
+            print("")
+
+        # Cuando se terminan las otras funciones:
+        print("\n Abriendo menú de turnos...")
+        print("Menú de gestión de turnos: \n 0. Volver al menú principal \n 1. Registrar nuevo turno \n 2. Finalizar turno \n 3. Ver turnos activos \n 4. Ver turnos finalizados \n 5. Ver estadísticas")
+        opcionMenu = int(input("Ingrese el número correspondiente para acceder: "))
+    
+    print("Volviendo para atrás... \n")
 
 def menuInicio():
     print("\n Menu principal")
-    print(" Menú de opciones: \n 0. Finalizar programa. \n 1. Usuarios \n 2. Turnos.")
-    opcionMenu = int(input("Ingrese el número correspondiente para acceder: "))  #Variable para finalizar programa o volver para atrás
+    print("Menú de opciones: \n 0. Finalizar programa. \n 1. Gestión de usuarios \n 2. Gestión de turnos.")
+    opcionMenu = int(input("Ingrese el número correspondiente para acceder: ")) 
     while opcionMenu != 0:
         while opcionMenu != 0 and (opcionMenu < 1 or opcionMenu > 2): #Validación de la opción de Menú
             if opcionMenu != 0:
                 print("\n Error. Seleccione una de las opciones siguientes.")
-                print(" Menú de opciones: \n 0. Finalizar programa. \n 1. Usuarios \n 2. Turnos.")
+                print("Menú de opciones: \n 0. Finalizar programa. \n 1. Gestión de usuarios \n 2. Gestión de turnos.")
                 opcionMenu = int(input("Ingrese el número correspondiente para acceder: "))
-
-        if opcionMenu == 1:
-            print("Opción 1.")
-            menuUsuarios()
-
-        elif opcionMenu == 2:
-            print("")
         
-        print(" Menú de opciones: \n 0. Finalizar programa. \n 1. Usuarios \n 2. Turnos.")
+        #Luego de validar opciones:
+        if opcionMenu == 1:
+            menuUsuarios()
+        elif opcionMenu == 2:
+            menuTurnos()
+        
+        # Cuando se terminan las otras funciones:
+        print("\n Menu principal")
+        print(" Menú de opciones: \n 0. Finalizar programa. \n 1. Gestión de usuarios \n 2. Gestión de turnos.")
         opcionMenu = int(input("Ingrese el número correspondiente para acceder: "))
     
     print("Finalizando programa")
 
-menuInicio()
+#Listas de usuarios:
+idUsuarios=[-1]
+nombreUsuarios=["Administrador"]
+contrasenasUsuarios=["-1"]
 
+#Listas de turnos
+idTurnos=[]
+idClienteTurno=[]
+horaLlegada=[]
+tipoTramite=[]
+estadosTurnos=[]
+
+login(idUsuarios, contrasenasUsuarios, nombreUsuarios)
+menuInicio()
